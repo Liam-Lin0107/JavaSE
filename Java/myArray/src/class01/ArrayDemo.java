ab0ffd0df459490ed73c12d017324e928b8edd43
Liam0107 <geffc1454@gmail.com>
2022-02-16 18:42:26 +0800

    first commit

--- Java/myArray/src/class01/ArrayDemo.java
+++ Java/myArray/src/class01/ArrayDemo.java
@@ -6,6 +6,9 @@ public class ArrayDemo {
         int[] arr = new int[3];
         // 輸出數組名稱
         System.out.println(arr); // [I@4617c264 : 為內存空間的位置名稱
+
+
+
         // 輸出數組中的元素
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
